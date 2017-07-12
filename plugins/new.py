b2858b93b1e6883c3a0510ef7716246fd1813c4c
Niko Pon <627837402@qq.com>
2017-07-12 14:21:33 +0800

    Update new.py

--- plugins/new.py
+++ plugins/new.py
@@ -11,10 +11,10 @@ import datetime
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url1="https://wds.modian.com/show_weidashang_pro/5329#1"
-url2="https://wds.modian.com/ranking_list?pro_id=5329"
+url1="https://wds.modian.com/show_weidashang_pro/5329#1"  #目标页面
+url2="https://wds.modian.com/ranking_list?pro_id=5329"    #目标页面聚聚榜
 headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-r1=requests.get(url1,verify=False,headers=headers)
+r1=requests.get(url1,verify=False,headers=headers)  
 r2=requests.get(url2,verify=False,headers=headers)
 
 html_doc_1=r1.text
@@ -23,7 +23,7 @@ soup1=BS4(html_doc_1,"html.parser")
 soup2=BS4(html_doc_2,"html.parser")
 global base
 base=" "
-def onQQMessage(bot, contact, member, content):
+def onQQMessage(bot, contact, member, content):   #发送消息的回调函数
     if content == 'wds':
         bot.SendTo(contact, July_wds.start())
     elif content == '排行':
@@ -31,8 +31,8 @@ def onQQMessage(bot, contact, member, content):
     elif '@ME' in content:
         bot.SendTo(contact,member.name+"初次见面，请多指教！！")
 
-@qqbotsched(hour='0-23', minute='0-59/1')
-def mytask(bot):
+@qqbotsched(hour='0-23', minute='0-59/1')   #定时函数 每分钟刷新 装饰器相关编写规则请参考qqbot
+def mytask(bot):  #jz信息实时更新
     global base
     url1="https://wds.modian.com/show_weidashang_pro/5329#1"
     r1=requests.get(url1,verify=False,headers=headers)
@@ -51,7 +51,7 @@ def mytask(bot):
             else:
                 print("equal_before:"+base)
                 print("equal_after:"+base)
-def return_top():
+def return_top():   #获取最新jz用户的ID
     url1="https://wds.modian.com/show_weidashang_pro/5329#1"
     r1=requests.get(url1,verify=False,headers=headers)
     html_doc_1=r1.text
@@ -59,7 +59,7 @@ def return_top():
     res=soup1.find_all("span","nick")[0].get_text()
     return res
 
-def return_ans(result):
+def return_ans(result):  #结果判断
     url1="https://wds.modian.com/show_weidashang_pro/5329#1"
     r1=requests.get(url1,verify=False,headers=headers)
     html_doc_1=r1.text
@@ -84,7 +84,7 @@ def return_ans(result):
         if(result==nick_sup[i]):
             return ("刚刚"+result+"聚聚 "+money+"！"+"在聚聚榜上排名第"+str(i+1)+"位！"+"\n"+"现在共有"+str(num)+"个人参与了活动。"+"\n"+"距离今日集资目标【￥"+str(fond)+"】还差【￥"+str(sub)+ "】\n"+"在最后的这段日子里，我们再坚持一下！"+"\n"+"wds链接：https://wds.modian.com/show_weidashang_pro/5329#1")
 
-@qqbotsched(hour='8-23', minute='0-59/30')
+@qqbotsched(hour='8-23', minute='0-59/30')#定时消息发送
 def mytask2(bot):
     gl = bot.List('group', '556592071')
     if gl is not None:
