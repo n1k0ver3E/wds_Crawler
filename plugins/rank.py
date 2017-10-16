c752b8d6db10c2dc63a20a85b38dd51d4692328f
Niko Pon <627837402@qq.com>
2017-10-16 15:16:03 -0500

    Update rank.py

--- plugins/rank.py
+++ plugins/rank.py
@@ -1,39 +1,34 @@
-def start():
-	import requests
-	from bs4 import BeautifulSoup as BS4
-	import requests
-	import datetime
-	from requests.packages.urllib3.exceptions import InsecureRequestWarning
-	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-	url1="https://wds.modian.com/show_weidashang_pro/5329#1"
-	url2="https://wds.modian.com/ranking_list?pro_id=5329"
-	headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-	r1=requests.get(url1,verify=False,headers=headers)
-	r2=requests.get(url2,verify=False,headers=headers)
-	i=0
-	result="*******BEJ48-李想7月应援*******"+'\n'
-	html_doc_1=r1.text
-	html_doc_2=r2.text
-	soup1=BS4(html_doc_1,"html.parser")
-	soup2=BS4(html_doc_2,"html.parser")
+import requests
+from qqbot.utf8logger import DEBUG
+from qqbot import qqbotsched
+from bs4 import BeautifulSoup as BS4
+import requests
+import datetime
+import re
+import time
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import b50
 
-	nick_sup=[]
-	money_sup=[]
-	rg=soup1.find("div",class_="b").get_text()
-	num=int(rg[0:3])
-	if num>=20:
-		num=20
+def main():
+    soup=b50.crawel_jujurank_url()
+    res=[]
+    results=soup.find_all("li","line1px")
+    for result in results:
+        res.append(result)
+    length=int(b50.return_support_num())
+    if length>=20:
+        length=20
+    rank=[[] for i in range(length)]
+    for i in range(length):
+        for link in res[i].find_all('a'):
+            user_id = re.sub('[^0-9.]',"",link.get("href"))
+            rank[i].append(user_id)
 
-	for i in range(num):
-		money=soup2.find_all("span","money")[i].get_text()
-		nick=soup2.find_all("span","nickname")[i].get_text()
-		nick_sup.append(nick)
-		money_sup.append(money)
+        user_name=res[i].find_all('span','nickname')[0].get_text()
+        rank[i].append(user_name)
 
-	for i in range(num):
-		result=result+"第"+str(i+1)+"位："+nick_sup[i]+ money_sup[i]+'\n'
-	print(result)	
-	return result
-
-if __name__ == '__main__':
-	start()
+        user_money=res[i].find_all('span','money')[0].get_text()
+        user_money = re.sub('[^0-9.]',"",user_money)
+        rank[i].append(user_money)
+    return rank    
