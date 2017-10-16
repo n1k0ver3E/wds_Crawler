47864fd94cf4055caf31a38d7ce087956f4b933e
Niko Pon <627837402@qq.com>
2017-10-16 15:13:51 -0500

    Update b50.py

--- plugins/b50.py
+++ plugins/b50.py
@@ -1,35 +1,37 @@
-import July_wds
 import check
-import rank
+import requests
 from qqbot.utf8logger import DEBUG
 from qqbot import qqbotsched
-import requests
-import wds_check
 from bs4 import BeautifulSoup as BS4
 import requests
 import datetime
 import re
 import time
+import rank
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url1="https://wds.modian.com/show_weidashang_pro/8098#1"
-url2="https://wds.modian.com/ranking_list?pro_id=8098"
-headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-r1=requests.get(url1,verify=False,headers=headers)
-r2=requests.get(url2,verify=False,headers=headers)
+#主爬虫
+def crawel_wds_url():  
+    url1="https://wds.modian.com/show_weidashang_pro/8098#1"
+    headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+    r1=requests.get(url1,verify=False,headers=headers)
+    html_doc_1=r1.text
+    soup=BS4(html_doc_1,"html.parser")
+    return soup
 
-html_doc_1=r1.text
-html_doc_2=r2.text
-soup1=BS4(html_doc_1,"html.parser")
-soup2=BS4(html_doc_2,"html.parser")
-global base
-base=" "
+#聚聚榜爬虫
+def crawel_jujurank_url():
+    url2="https://wds.modian.com/ranking_list?pro_id=8098"
+    headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+    r2=requests.get(url2,verify=False,headers=headers)
+    html_doc_2=r2.text
+    soup=BS4(html_doc_2,"html.parser")
+    return soup
+
 def onQQMessage(bot, contact, member, content):
-    if content == 'wds':
-        bot.SendTo(contact, July_wds.start())
-    elif content == '排行':
-        bot.SendTo(contact,rank.start())
+    if content == 'b50' or 'B50' or 'b50 ':
+        bot.SendTo(contact, Ontime())
     elif content == 'flag':
         bot.SendTo(contact,flag())
     elif content == '口袋':
@@ -37,95 +39,97 @@ def onQQMessage(bot, contact, member, content):
     elif '@ME' in content:
         bot.SendTo(contact,member.name+"初次见面，请多指教！！")
 
+def Ontime():
+    money=return_total_money()
+    support_num=return_support_num()
+    return("**脑门儿的集资播报：**"+"\n"+"当前活动已筹："+money+"元"+"\n"+"参与人数："+support_num+"\n"+"wds链接：http://jli.li/I"+"\n"+"**B50就要到了,行行好吧！**")
+
 def flag():
     return("今天还有2名义士立下了追加Flag，希望大家努力让他们早日拔旗～"+"\n"+"① 现在参与人数为【18】人，截止到14日24点整，绫鱼按照集资打卡人数（需单笔不少于2.5）乘以2.5追加，如果集资金额达到【3000】，额外追加【200】。"+"\n"+"② 从集资总额达到【2000】开始，截止到18日，按照集资金额，每增加【32】，残花追投一票，上限【8000元整】。"+"\n"+"大家千万不要放过他们哟～想推有理想！")
 
 def koudai():
     return("脑门儿的口袋提醒：本周口袋48的公社图文社区，有参与开屏活动的投稿，大家记得每日打开app签到领取免费鸡腿，并进去文稿赠送，让我们一起努力为李想拿下本周的开屏奖励！")
 
-def test():
-    g = bot.List('group', "456班")[0]
-    print (bot.List(g))
-
+#定时任务
+@qqbotsched(hour='9-23/4')
+def b50xingxinghao(bot):
+    money=return_total_money()
+    post_text="B50就要到了，不来打个卡么？"+"\n"+"当前活动已筹："+money+"元"+"\n"+"wds链接：http://jli.li/I"
+    gl = bot.List('group', '389660866')
+    if gl is not None:
+        for group in gl:
+            bot.SendTo(group,post_text)
+#监听任务
 @qqbotsched(hour='0-23', minute='0-59', second='0-59/30')
 def mytask(bot):
-    global base
-    url1="https://wds.modian.com/show_weidashang_pro/8098#1"
-    r1=requests.get(url1,verify=False,headers=headers)
-    html_doc_1=r1.text
-    soup1=BS4(html_doc_1,"html.parser")
-    gl = bot.List('group', '556592071')
+    gl = bot.List('group', '389660866')
     if gl is not None:
         for group in gl:
             result=return_top()
+            print("Status：",result)
             if(result != -1):
-                bot.SendTo(group,return_ans(result))
+                bot.SendTo(group,return_ans(result[0],result[1]))
         
             
 def return_top():
-	url1="https://wds.modian.com/show_weidashang_pro/8098#1"
-	headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-	temp_array=[]
-	r1=requests.get(url1,verify=False,headers=headers)
-	html_doc_1=r1.text
-	soup=BS4(html_doc_1,"html.parser")
-	soup1=soup.li
-	result=soup1.find_all("a", attrs={"class": "add-jubao"})[0]
-	#print(result)
-	comid=result.get('to_comid')
-	#print(comid)
-	temp_array.append(comid)
+    temp_array=[]
+    soup=crawel_wds_url()
+    soup1=soup.li
+    result=soup1.find_all("a", attrs={"class": "add-jubao"})[0]
+    comid=result.get('to_comid')   #交易ID
+    temp_array.append(comid)
+    userid=result.get('to_user')    #用户ID
+    temp_array.append(userid)
+    res=soup1.find_all("span","nick")[0].get_text()    #用户名
+    temp_array.append(res)
+    print(temp_array)
 
-	res=soup1.find_all("span","nick")[0].get_text()
-	#print(res)
-	temp_array.append(res)
-	print(temp_array)
+    f=open("/Users/Niko/.qqbot-tmp/plugins/b50_msg",'r')
 
-	f=open("/Users/Niko/.qqbot-tmp/plugins/b50_msg",'r')
+    if(temp_array[0]!=f.read()):
+        f=open("/Users/Niko/.qqbot-tmp/plugins/b50_msg",'w')
+        f.write(temp_array[0])
+        post_id=temp_array[1]
+        post_name=temp_array[2]
+        f.close()
+        return (post_name,post_id)
+    f.close()
+    return (-1)
 
-	if(temp_array[0]!=f.read()):
-		f=open("/Users/Niko/.qqbot-tmp/plugins/b50_msg",'w')
-		f.write(temp_array[0])
-		post_name=temp_array[1]
-		f.close()
-		return (post_name)
-	f.close()
-	return (-1)
+def return_ans(username,userid):
+    money = str(return_user_support_money())
+    ranking = str(return_user_ranking(userid))
+    total = str(return_total_money())
+    support_num = str(return_support_num())
+    return("刚刚 "+username+" 聚聚 "+"在【第四届金曲大赏集资2.0】活动中"+money+"！"+"\n"+"在聚聚榜上排名第"+ranking+"位!"+"\n"+"参与人数："+support_num+"\n"+"活动累计："+total+"元"+"\n"+"wds链接：http://jli.li/I")
 
-
-def return_ans(result):
-    url1="https://wds.modian.com/show_weidashang_pro/8098#1"
-    r1=requests.get(url1,verify=False,headers=headers)
-    html_doc_1=r1.text
-    soup1=BS4(html_doc_1,"html.parser")
-
-    nick_sup=[]
-    money_sup=[]
-    rg=soup1.find("div",class_="b").get_text()
-    num=int(rg[0:2])
-    print(num)
-    people=num
-    if num>=20:
-        num=20
-    print (people)
-    fond=4000
-    res=soup1.find_all("div", class_="mon current")[0].find_all("span")[1].get_text()
-    res=res[1:]
-    res=float(res.replace(',',""))
-    sub=round(fond-res,2)
-    if sub<=0:
-        sub=0
-
-
+def return_user_ranking(userid):
+    userId=int(userid)
+    rank=rank.main()
+    num=int(return_support_num())
     for i in range(num):
-        nick=soup2.find_all("span","nickname")[i].get_text()
-        nick_sup.append(nick)
-        money=soup1.find_all("span","nick_sup")[0].get_text()
-    for i in range(num):
-        if(result==nick_sup[i]):
-            print (i)
-            return ("刚刚"+result+"聚聚 "+money+"！"+"在聚聚榜上排名第"+str(i+1)+"位！"+"\n"+"wds链接：http://jli.li/I")
-        else:
-            return ("刚刚"+result+"聚聚 "+money+"！"+"\n"+"现在共有"+str(people)+"个人参与了活动。"+"wds链接：http://jli.li/I")
+        rankId=int(rank[i][0])
+        if(userId == rankId):
+            return i+1
+
+def return_user_support_money():
+    soup=crawel_wds_url()
+    money=soup.find_all("span","nick_sup")[0].get_text()
+    return money
+
+def return_support_num():
+    soup=crawel_wds_url()
+    support_text=soup.find("div",class_="b").get_text()
+    support_num = re.sub('[^0-9.]',"",support_text)
+    return support_num
+
+def return_total_money():
+    soup=crawel_wds_url()
+    total=soup.find_all("div", class_="mon current")[0].find_all("span")[1].get_text()
+    total_num = re.sub('[^a-zA-Z0-9.]',"",total)
+    return total_num
+
+
+
 
 
